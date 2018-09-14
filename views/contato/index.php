e3f64d1754b1ed4bdab893ffc3768d407ba1a337
Diego Fassion <Diego Fassion@VAIO>
2018-09-14 15:51:24 -0300

    Merge origin/master

--- views/contato/index.php
+++ views/contato/index.php
@@ -1,32 +1,34 @@
             <div id="page-wrapper">
-                <!-- Cabeçalho da página -->
-                <div class="row">
-                    <div class="col-lg-12">
-                        <h1 class="page-header">
-                            Contatos <small></small>
-                        </h1>
+                <div class="container-fluid">
+                    <!-- Cabeçalho da página -->
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <h1 class="page-header">
+                                Contatos 
+                            </h1>
+                        </div>
                     </div>
-                </div>
-		
-		<!-- CONTATOS -->
-                <div class="row">
-                    <!-- IMAGEM -->
-                    <div class="col-sm-3"></div>
-                    <div class="col-sm-5">
-                        <img src="<?=URL?>public/bs/img/contatosms.png" class="img-responsive">
-                    </div>
-                    <!-- LOCALIZAÇÃO -->
-                    <div class="col-sm-11" >
-			<div class="configdiv" style="text-align:center">
-                            <h3>Secretaria Municipal da Saúde</h3>
-                            <h4>NÚCLEO DE INFORMAÇÕES</h4>
-                            <P class=intro><br>
-                                Avenida Castro Alves, 61 - Centro<BR>
-                                CEP: 17.500–002 - Marília/SP <br>
-                                Fone: (14)3402-6500 <br>
-                                <A href="mailto:sistemas.saude@marilia.sp.gov.br">sistemas.saude@marilia.sp.gov.br</A>
-                            </P>
-			</div>
+
+                    <!-- CONTATOS -->
+                    <div class="row">
+                        <!-- IMAGEM -->
+                        <div class="col-sm-3"></div>
+                        <div class="col-sm-5">
+                            <img src="<?=URL?>public/bs/img/contatosms.png" class="img-responsive">
+                        </div>
+                        <!-- LOCALIZAÇÃO -->
+                        <div class="col-sm-11" >
+                            <div class="configdiv" style="text-align:center">
+                                <h3>Secretaria Municipal da Saúde</h3>
+                                <h4>NÚCLEO DE INFORMAÇÕES</h4>
+                                <P class=intro><br>
+                                    Avenida Castro Alves, 61 - Centro<BR>
+                                    CEP: 17.500–002 - Marília/SP <br>
+                                    Fone: (14)3402-6500 <br>
+                                    <A href="mailto:sistemas.saude@marilia.sp.gov.br">sistemas.saude@marilia.sp.gov.br</A>
+                                </P>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
--- views/footer.php
+++ views/footer.php
@@ -1,6 +1,21 @@
-
+        <!-- RODAPÉ -->
+        <footer class="footer">
+            <div class="container">
+                <div class="row"> <br>
+                    <div class="text-muted">
+                        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                            <p><span class="glyphicon glyphicon-circle-arrow-right"> e-Limites</span></p>
+                        </div>
+                        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                            <p>&REG; Copyright 2018 by Diego Fassion</p>
+                        </div>
+                        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                            <p>Secretaria Municipal da Saúde de Marília</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
      </div>
-
 </body>
-
 </html>

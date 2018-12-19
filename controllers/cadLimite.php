36c6df50ad5427a101963592eebb41e81976e231
Diego Fassion <Diego Fassion@VAIO>
2018-12-19 14:51:50 -0200

    Merge origin/master

---
+++ controllers/cadLimite.php
@@ -0,0 +1,45 @@
+<?php
+
+class cadLimite extends Controller{
+    
+    function __construct() {
+        parent::__construct();
+        Auth::autentica();
+        $this->view->js = array('views/cadLimite/limite.js');
+	//$this->view->js = array('views/cadLimite/buscaViaCep.js');
+        $this->view->angularApp="LimitesApp";
+    }
+    
+    function index() {
+        Session::init();
+	Session::set("itemativo","cadLimites");
+        $this->view->title = 'e-Limites - Cadastrar Limite';
+	$this->view->render('header');
+        $this->view->render('cadLimite/index');
+	$this->view->render('footer');
+    }
+    
+    function insert() {
+        $this->model->insert();
+    }
+    
+    function lista() {
+        $this->model->lista();
+    }
+	
+    function del($ra=null) {
+        $this->model->del($ra);
+    }
+	
+    function loadData($ra=null) {
+        $this->model->loadData($ra);
+    }
+	
+    function save() {
+        $this->model->save();
+    }
+    
+    function buscaUnid() {
+        $this->model->buscaUnid();
+    }
+}
---
+++ models/cadLimite_model.php
@@ -0,0 +1,85 @@
+<?php
+
+class cadLimite_model extends Model{
+    
+    public function __construct() {
+        parent::__construct();
+    }
+    
+    public function lista() {  
+        
+        $cep=$_POST["c"];
+        $result=$this->db->select('select id,cep,logradouro,num_inicial,num_final,bairro,unidade,area,micro_area from limite where cep=:par_cep',array(":par_cep"=>$cep));
+			
+        $result = json_encode($result);
+		
+	echo $result;
+    }
+	
+    public function del($ra=null) {  
+		
+        $ra=(int)$ra;
+        $this->db->delete('angular.aluno',"ra='$ra'");
+
+        echo "Aluno Removido com Sucesso!";
+    }
+	
+    public function loadData($ra=null) {  
+		
+        $ra=(int)$ra;
+        if($ra!=0){	
+            //alterei aqui tambem para retornar o nome dos 
+            //campos de acordo com os nomes definidos no ng-model
+            $result=$this->db->select('select ra as Ra,nome as Nome,endereco as End from angular.aluno where ra=:ra',array(":ra"=>$ra));
+            $result = json_encode($result);
+            echo($result);
+        }
+    }
+	
+    public function salvar() { //insert
+		
+        /*
+	php://input --> permite ler os dados do corpo da requisicao http
+	No angular os dados são passados como json no corpo do http
+	*/
+	
+	$limite = json_decode(file_get_contents('php://input'),true);     
+		        
+        //$this->db->insert('angular.limite', array('ra' =>$aluno["Ra"],'nome'=>$aluno["Nome"],'endereco'=>$aluno["End"]));
+        
+        $this->db->insert('angular.limite', array('cep'=>$cep["Cep"],'logradouro'=>$logradouro["Logradouro"],'num_inicial'=>$num_inicial["NumIni"],
+                          'num_final'=>$num_final["NumFim"],'bairro'=>$bairro["Bairro"],'unidade'=>$unidade["Unidade"],'area'=>$area["Area"],
+                          'micro_area'=>$micro_area["MicroArea"]));
+        
+        echo "Dados Inseridos com Sucesso";
+    }    
+    
+    public function save() {  //EDITAR
+	
+        $limite = json_decode(file_get_contents('php://input'),true);     
+		
+	//$ra=(int)$aluno["Ra"];
+	$cep=$limite["Cep"];
+	$logradouro=$limite["Logradouro"];
+        $num_inicial=$limite["NumIni"];
+        $num_final=$limite["NumFim"];
+        $bairro=$limite["Bairro"];
+        $unidade=$limite["Unidade"];
+        $area=$limite["Area"];
+        $micro_area=$limite["MicroArea"];
+	
+	$dadosSave=array('cep'=>$cep,'logradouro'=>$logradouro,'num_inicial'=>$num_inicial,'num_final'=>$num_final,'bairro'=>$bairro,'unidade'=>$unidade,'area'=>$area,'micro_area'=>$micro_area);
+		
+	//print_r($limite);		        
+        $this->db->update('angular.aluno', $dadosSave,"ra='$ra'");
+        echo "Dados gravados com Sucesso";   
+    }   
+    
+    public function buscaUnidades() {
+        $resultado_unidades=$this->db->select('select cnes, descricao from unidade order by descricao');
+		
+	$resultado_unidades = json_encode($resultado_unidades);
+		
+	echo $resultado_unidades;
+    }
+}
